6ef1eacd2be4b8bb192f01f49c63ee1efc9f1f84
Anilkumar-Peepul <anil.pallekadi@peepulagri.com>
2026-05-11 15:19:25 +0530

    Update mqtt_handler.py

--- mqtt_handler.py
+++ mqtt_handler.py
@@ -50,7 +50,6 @@ class MQTTHandler:
             return
 
         if "motor_control" in msg.topic:
-            print("Handle Motor Controlling")
             self.loop.create_task(self.handle_motor_control_message(payload))
         elif "mode_change" in msg.topic:
             self.loop.create_task(self.handle_mode_change(payload))
